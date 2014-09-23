22b86d00ecc9d5f19f484bf8a78b2c2a80c8123d
r2k0 <iheartcpp@gmail.com>
2014-09-22 22:54:41 -0700

    adding scripts

--- urllib/t1.py
+++ urllib/t1.py
@@ -1,7 +1,7 @@
 import urllib
 import re
 
-urls = ["http://finance.yahoo.com","http://google.com/finance"]
+urls = ["http://finance.yahoo.com","http://google.com/finance","http://bloomberg.com","http://finviz.com"]
 
 regex = '<title>(.+?)</title>'
 pattern = re.compile(regex)
